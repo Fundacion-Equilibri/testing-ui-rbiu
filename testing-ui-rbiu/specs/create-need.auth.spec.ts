9828233d966c21816215dfce832c62e7d2570f34
unknown <cperka@fundacioequilibri.org>
2025-10-08 21:33:12 +0200

    Testing de la pagina crea una necesidad

--- testing-ui-rbiu/specs/create-need.auth.spec.ts
+++ testing-ui-rbiu/specs/create-need.auth.spec.ts
@@ -5,16 +5,19 @@ import { CreateNeedPage, type NeedData } from "../pages/create-need-page";
 import { MyNeedsPage } from "../pages/my-needs-page";
 import { EditNeedPage } from "../pages/edit-need-page";
 
-test.describe("Página de crear un producto /crea-un-producto (Auth)", () => {
+test.describe("Página de crear una necesidad /crea-una-necesidad (Auth)", () => {
   test.describe("Flujo exitoso", () => {
-    let product: NeedData;
+    let need: NeedData;
     let myNeedsPage: MyNeedsPage;
     let editNeedPage: EditNeedPage;
 
-    test.beforeEach(() => {
-      const productName = "Aeronave F-22 Raptor " + Date.now();
-      product = {
-        name: productName,
+    test.beforeEach(({ page }) => {
+      myNeedsPage = new MyNeedsPage(page);
+      editNeedPage = new EditNeedPage(page);
+
+      const needName = "Aeronave F-22 Raptor " + Date.now();
+      need = {
+        name: needName,
         minPrice: "5000",
         maxPrice: "10000",
         needDetails: "Aviones de combate utilizados en misiones de prueba.",
@@ -25,14 +28,10 @@ test.describe("Página de crear un producto /crea-un-producto (Auth)", () => {
       };
     });
 
-    test.beforeEach(() => {});
-
     test.afterEach(async ({ page }) => {
-      const myNeedsPage = new MyNeedsPage(page);
-      const editNeedPage = new EditNeedPage(page);
-
+      // Limpieza: se elimina la necesidad creada para mantener el entorno limpio.
       await myNeedsPage.goto();
-      await myNeedsPage.editNeed(product.name);
+      await myNeedsPage.editNeed(need.name);
       await editNeedPage.verifyPageLoaded();
       await editNeedPage.deleteNeed();
     });
@@ -41,10 +40,10 @@ test.describe("Página de crear un producto /crea-un-producto (Auth)", () => {
       const createNeedPage = new CreateNeedPage(page);
       await createNeedPage.goto();
       await createNeedPage.handleCookies();
-      await createNeedPage.fillForm(product);
+      await createNeedPage.fillForm(need);
       await createNeedPage.submit();
 
-      await createNeedPage.verifySuccess(product.name);
+      await createNeedPage.verifySuccess(need.name);
     });
 
     test("Debería mostrar un error al intentar crear una necesidad con un nombre duplicado", async ({
@@ -55,13 +54,13 @@ test.describe("Página de crear un producto /crea-un-producto (Auth)", () => {
       // --- 1. Crear la necesidad inicial ---
       await createNeedPage.goto();
       await createNeedPage.handleCookies();
-      await createNeedPage.fillForm(product);
+      await createNeedPage.fillForm(need);
       await createNeedPage.submit();
-      await createNeedPage.verifySuccess(product.name);
+      await createNeedPage.verifySuccess(need.name);
       
-      // --- 2. Intentar crear el mismo producto de nuevo ---
+      // --- 2. Intentar crear la misma necesidad de nuevo ---
       await createNeedPage.goto(); // Volvemos a la página de creación
-      await createNeedPage.fillForm(product);
+      await createNeedPage.fillForm(need);
       await createNeedPage.submit();
       // Verificar el mensaje de error de nombre duplicado
 
@@ -87,7 +86,7 @@ test.describe("Página de crear un producto /crea-un-producto (Auth)", () => {
     const validationTestCases = [
       {
         case: "cuando todos los campos están vacíos",
-        productData: {
+        needData: {
           ...baseNeed,
           name: "",
           minPrice: "",
@@ -106,37 +105,42 @@ test.describe("Página de crear un producto /crea-un-producto (Auth)", () => {
         ],
       },
       {
-        case: "cuando el nombre está vacío",
-        productData: { ...baseNeed, name: "" },
+        case: "cuando el nombre de la necesidad está vacío",
+        needData: { ...baseNeed, name: "" },
         expectedError: "¿Qué necesitas?: Este campo es obligatorio.",
       },
       {
+        case: "cuando el detalle de la necesidad está vacía",
+        needData: { ...baseNeed, needDetails: "" },
+        expectedError: "Detalla un poco más lo que necesitas: Este campo es obligatorio.",
+      },
+      {
+        case: "cuando la razon o el motivo de la necesidad está vacía",
+        needData: { ...baseNeed, reasonNeed: "" },
+        expectedError: "Explícanos que uso le vas a dar y el motivo de la necesidad: Este campo es obligatorio.",
+      },
+      {
         case: "cuando el precio es inferior a 1",
-        productData: { ...baseNeed, minPrice: "0" },
+        needData: { ...baseNeed, minPrice: "0" },
         expectedError:
           "Precio mínimo (en logos): El precio mínimo no puede ser inferior a 1 λ.",
       },
       {
-        case: "cuando la descripción está vacía",
-        productData: { ...baseNeed, needDetails: "" },
-        expectedError: "Detalla un poco más lo que necesitas: Este campo es obligatorio.",
-      },
-      {
         case: "cuando no se sube una imagen",
-        productData: { ...baseNeed, imagePath: "" }, // Asumimos que un path vacío significa no subir imagen
+        needData: { ...baseNeed, imagePath: "" }, // Asumimos que un path vacío significa no subir imagen
         expectedError: "Imagen: Este campo es obligatorio.",
       },
     ];
 
     for (const tc of validationTestCases) {
-      test(`debería mostrar un error ${tc.case}`, async ({ page }) => {
+      test(`debería mostrar error ${tc.case}`, async ({ page }) => {
         const createNeedPage = new CreateNeedPage(page);
         await createNeedPage.goto();
         await createNeedPage.handleCookies();
         // Usamos un 'if' para decidir si llamamos a setInputFiles
         await createNeedPage.fillForm(
-          tc.productData,
-          !!tc.productData.imagePath
+          tc.needData,
+          !!tc.needData.imagePath
         );
 
         await createNeedPage.submit();
